77706c2ac3445b1c904c14e5186628ec864e1c67
robomatix <robomatix@robomatix.net>
2014-12-07 06:56:59 +0100

    Basic preload !

---
+++ js/game/states/Preload.js
@@ -0,0 +1,27 @@
+tbsps.Preload = function() {
+    this.ready = false;
+};
+
+tbsps.Preload.prototype = {
+    preload: function() {
+
+        this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'preloadbar');
+        this.preloadBar.anchor.setTo(0.5);
+
+        this.load.setPreloadSprite(this.preloadBar);
+
+        this.load.onLoadComplete.add(this.onLoadComplete, this);
+
+        this.load.image('square', 'assets/images/square.png');
+
+    },
+    create: function() {
+        this.preloadBar.cropEnabled = false;
+    },
+    update: function() {
+
+    },
+    onLoadComplete: function() {
+        this.ready = true;
+    }
+};
--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
+'use strict';
+
+//global variables
+window.onload = function () {
+    var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '');
+
+    // Game States
+    game.state.add('Boot', tbsps.Boot);
+    game.state.add('Preload', tbsps.Preload);
+
+
+    game.state.start('Boot');
+};
