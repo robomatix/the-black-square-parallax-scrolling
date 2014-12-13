cd9a05745706041f73cc547b0819d1dbe845341b
robomatix <robomatix@robomatix.net>
2014-12-13 08:18:43 +0100

    Game & MainMenu : Top Parallax

--- js/game/states/Game.js
+++ js/game/states/Game.js
@@ -10,6 +10,23 @@ tbsps.Game = function() {
 tbsps.Game.prototype = {
     create: function() {
 
+        this.backgroundTop1 = this.game.add.tileSprite(0, 0, this.game.width, 100, 'halfRound');
+        this.backgroundTop1.alpha = 0.1;
+        this.backgroundTop1.autoScroll(-50, 0);
+
+        this.backgroundTop2 = this.game.add.tileSprite(0, -50, this.game.width, 100, 'halfRound');
+        this.backgroundTop2.alpha = 0.3;
+        this.backgroundTop2.autoScroll(-100, 0);
+
+        this.backgroundTop3 = this.game.add.tileSprite(0, -75, this.game.width, 100, 'halfRound');
+        this.backgroundTop3.alpha = 0.5;
+        this.backgroundTop3.autoScroll(-150, 0);
+
+        this.backgroundTopRotLeft1 = this.game.add.tileSprite(-10, 0, this.game.width, 99, 'halfRound');
+        this.backgroundTopRotLeft1.rotation = -0.1;
+        this.backgroundTopRotLeft1.alpha = 0.2;
+        this.backgroundTopRotLeft1.autoScroll(-75, 0);
+
 
 
         this.backgroundBottom1 = this.game.add.tileSprite(0, this.game.height-150, this.game.width, 150, 'triangle');
@@ -24,10 +41,10 @@ tbsps.Game.prototype = {
         this.backgroundBottom3.alpha = 0.5;
         this.backgroundBottom3.autoScroll(-150, 0);
 
-        this.backgroundRotLeft1 = this.game.add.tileSprite(0, this.game.height-150, this.game.width, 150, 'triangle');
-        this.backgroundRotLeft1.rotation = 0.1;
-        this.backgroundRotLeft1.alpha = 0.2;
-        this.backgroundRotLeft1.autoScroll(-75, 0);
+        this.backgroundBottomRotLeft1 = this.game.add.tileSprite(0, this.game.height-150, this.game.width, 148, 'triangle');
+        this.backgroundBottomRotLeft1.rotation = 0.1;
+        this.backgroundBottomRotLeft1.alpha = 0.2;
+        this.backgroundBottomRotLeft1.autoScroll(-75, 0);
 
 
 
--- js/game/states/MainMenu.js
+++ js/game/states/MainMenu.js
@@ -3,15 +3,32 @@ tbsps.MainMenu = function() {};
 tbsps.MainMenu.prototype = {
     create: function() {
 
+        this.backgroundTop1 = this.game.add.tileSprite(0, 0, this.game.width, 100, 'halfRound');
+        this.backgroundTop1.alpha = 0.1;
+        this.backgroundTop1.autoScroll(-50, 0);
+
+        this.backgroundTop2 = this.game.add.tileSprite(0, -50, this.game.width, 100, 'halfRound');
+        this.backgroundTop2.alpha = 0.3;
+        this.backgroundTop2.autoScroll(-100, 0);
+
+        this.backgroundTop3 = this.game.add.tileSprite(0, -75, this.game.width, 100, 'halfRound');
+        this.backgroundTop3.alpha = 0.5;
+        this.backgroundTop3.autoScroll(-150, 0);
+
+        this.backgroundTopRotLeft1 = this.game.add.tileSprite(-10, 0, this.game.width, 99, 'halfRound');
+        this.backgroundTopRotLeft1.rotation = -0.1;
+        this.backgroundTopRotLeft1.alpha = 0.2;
+        this.backgroundTopRotLeft1.autoScroll(-75, 0);
+
         this.backgroundBottom1 = this.game.add.tileSprite(0, this.game.height-150, this.game.width, 150, 'triangle');
         this.backgroundBottom1.alpha = 0.1;
         this.backgroundBottom1.autoScroll(-50, 0);
 
-        this.backgroundBottom2 = this.game.add.tileSprite(0, this.game.height-75, this.game.width, 75, 'triangle');
+        this.backgroundBottom2 = this.game.add.tileSprite(0, this.game.height-75, this.game.width, 150, 'triangle');
         this.backgroundBottom2.alpha = 0.3;
         this.backgroundBottom2.autoScroll(-100, 0);
 
-        this.backgroundBottom3 = this.game.add.tileSprite(0, this.game.height-50, this.game.width, 50, 'triangle');
+        this.backgroundBottom3 = this.game.add.tileSprite(0, this.game.height-50, this.game.width, 150, 'triangle');
         this.backgroundBottom3.alpha = 0.5;
         this.backgroundBottom3.autoScroll(-150, 0);
 
